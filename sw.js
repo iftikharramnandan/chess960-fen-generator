70604d7dae8994b676d130a07ebba5f22becfcad
iftikharramnandan <122757261+iftikharramnandan@users.noreply.github.com>
2025-08-04 07:17:12 -0400

    Add service worker for offline support

---
+++ sw.js
@@ -0,0 +1,27 @@
+// sw.js
+
+const CACHE_NAME = 'fen-generator-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
